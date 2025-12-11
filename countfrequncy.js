cf4b9c245d31fe63c937802294a58e6e0899d7ae
KUNALSHRIMAL <2019pietcskunal86@poornima.org>
2025-12-11 13:04:54 +0530

    11-12-25

---
+++ countfrequncy.js
@@ -0,0 +1,11 @@
+// function strfrq(str) {
+//     let count=0;
+//     for(let i=0;i<str.length;i++){
+//         if (str[]) {
+            
+//         }
+//     }
+
+// }
+str="kunal"
+console.log(str.split(""))
---
+++ reversestring.js
@@ -0,0 +1,8 @@
+function strrevrse(str) {
+    let rev=str.split("").reverse().join("");
+    console.log(`The reverse of ${str} is ${rev}`)
+}
+strrevrse("kunal")
+
+const arr=[12,54,85,98,74,32,10];
+console.log(arr.reverse())
