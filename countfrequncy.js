4efc53435eea2c2e21b7e2ef25b49bc96edb998d
KUNALSHRIMAL <2019pietcskunal86@poornima.org>
2025-12-11 14:59:56 +0530

    11-12-25

--- countfrequncy.js
+++ countfrequncy.js
@@ -1,11 +1,33 @@
-// function strfrq(str) {
-//     let count=0;
-//     for(let i=0;i<str.length;i++){
-//         if (str[]) {
-            
-//         }
-//     }
+function countCharacterFrequencies(str) {
+  const frequencies = new Map();
 
+  // Iterate over the string character by character
+  for (const char of str) {
+    // Get the current count, or 0 if not present, then increment
+    frequencies.set(char, (frequencies.get(char) || 0) + 1);
+  } 
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                           
+  return frequencies;
+}
+
+const inputString = "hello world";
+const result = countCharacterFrequencies(inputString);
+console.log(result);
+// Output: Map(8) { 'h' => 1, 'e' => 1, 'l' => 3, 'o' => 2, ' ' => 1, 'w' => 1, 'r' => 1, 'd' => 1 }
+
+
+// function countCharacterFrequenciesReduce(str) {
+//   // Split the string into an array of characters
+//   const frequencies = str.split('').reduce((acc, char) => {
+//     // Check if the character key exists in the accumulator (acc)
+//     acc[char] = (acc[char] || 0) + 1;
+//     return acc;
+//   }, {}); // Start with an empty object as the initial accumulator
+
+//   return frequencies;
 // }
-str="kunal"
-console.log(str.split(""))
+
+// const inputStringReduce = "javascript";
+// const resultReduce = countCharacterFrequenciesReduce(inputStringReduce);
+// console.log(resultReduce);
+// // Output: { j: 1, a: 2, v: 1, s: 1, c: 1, r: 1, i: 1, p: 1, t: 1 }
---
+++ removedupicate.js
@@ -0,0 +1,3 @@
+const arr=[12,54,85,5,98,74,32,10,10];
+const newarr=[...new Set(arr)];
+console.log('unique', newarr);
